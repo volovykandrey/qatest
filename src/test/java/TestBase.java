a23dc7774fd5b32dcd58640cd5b8d766d6261172
Andrii Volvyk <andrii@signalvine.com>
2020-04-28 20:41:54 +0300

    [master] update pom.xml

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,5 +1,3 @@
-import io.github.bonigarcia.wdm.ChromeDriverManager;
-import io.github.bonigarcia.wdm.DriverManagerType;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
